a86f2f5123ba3e197d7c566937ecb108c6e87032
Dorffdaniel <danielcontabill@hotmail.com>
2024-12-23 19:10:20 +0000

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+let changeColor = document.querySelector('.changeColor');
+let body = document.querySelector('.body');
+let corBranca = document.querySelectorAll('h1','h2', 'input');
+
+let textoOriginal = changeColor.textContent;
+
+changeColor.addEventListener('click', ()=>{ 
+    body.classList.toggle('newBackground')
+    if(textoOriginal == textContent){
+        
+    }
+})
