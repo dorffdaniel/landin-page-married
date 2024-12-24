7a2e0cb8bb20ba1f95a14e26635e54fc66161b21
Dorffdaniel <danielcontabill@hotmail.com>
2024-12-24 19:54:32 +0000

    btn dark finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,21 @@
 let changeColor = document.querySelector('.changeColor');
 let body = document.querySelector('.body');
-let corBranca = document.querySelectorAll('h1','h2', 'input');
+let whiteColor = document.querySelectorAll('h1, h2, input, i');
 
 let textoOriginal = changeColor.textContent;
 
 changeColor.addEventListener('click', ()=>{ 
+
     body.classList.toggle('newBackground')
-    if(textoOriginal == textContent){
-        
+    if(changeColor.textContent === textoOriginal){
+        changeColor.textContent='light version'
+        changeColor.classList.add('h1')
+    }else{
+        changeColor.textContent = textoOriginal;
+        changeColor.classList.remove('h1')
     }
+
+    whiteColor.forEach((percorre)=>{
+        percorre.classList.toggle('coresBrancas')
+    })
 })
